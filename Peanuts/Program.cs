a3fee642ccd46821aad1fa734a9c179fb5b0c2fc
JLK5 <jlkborkowski@gmail.com>
2023-03-20 12:37:06 +0100

    Added D20

--- Peanuts/Program.cs
+++ Peanuts/Program.cs
@@ -2,6 +2,7 @@
 Console.WriteLine("1. D3");
 Console.WriteLine("2. D4");
 Console.WriteLine("3. D6");
+Console.WriteLine("4. D20");
 string choice = Console.ReadLine();
 Random rng = new Random();
 int randomthrow = 0;
@@ -18,6 +19,10 @@ switch(choice)
         break;
     case "3":
         randomthrow = rng.Next(1, 6);
+        Console.WriteLine("Wyrzuciłeś: " + randomthrow.ToString());
+        break;
+    case "4":
+        randomthrow = rng.Next(1, 20);
         Console.WriteLine("Wyrzuciłeś: " + randomthrow.ToString());
         break;
     default:
