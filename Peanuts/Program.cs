ccc32d7785e6827198799c1455bd331843ce68b2
JLK5 <jlkborkowski@gmail.com>
2023-03-20 12:27:28 +0100

    Dodaj pliki projektów.

---
+++ Peanuts/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Podaj kość jaką chcesz rzucić:");
+Console.WriteLine("1. D3");
+Console.WriteLine("2. D4");
+Console.WriteLine("3. D6");
+string choice = Console.ReadLine();
+Random rng = new Random();
+int randomthrow = 0;
+
+switch(choice)
+{
+    case "1":
+        randomthrow = rng.Next(1, 3);
+        Console.WriteLine("Wyrzuciłeś: " + randomthrow.ToString());
+        break;
+    case "2":
+        randomthrow = rng.Next(1, 4);
+        Console.WriteLine("Wyrzuciłeś: " + randomthrow.ToString());
+        break;
+    case "3":
+        randomthrow = rng.Next(1, 6);
+        Console.WriteLine("Wyrzuciłeś: " + randomthrow.ToString());
+        break;
+    default:
+        Console.WriteLine("Podałeś złą opcje!");
+        break;
+}
