8be39575e544173557f823329ac32c6823ba3c10
JLK5 <jlkborkowski@gmail.com>
2023-03-20 12:57:48 +0100

    Loop

--- Peanuts/Program.cs
+++ Peanuts/Program.cs
@@ -3,29 +3,36 @@ Console.WriteLine("1. D3");
 Console.WriteLine("2. D4");
 Console.WriteLine("3. D6");
 Console.WriteLine("4. D20");
-string choice = Console.ReadLine();
+Console.WriteLine("q. Wyjście");
 Random rng = new Random();
 int randomthrow = 0;
-
-switch(choice)
+string choice = "";
+do
 {
-    case "1":
-        randomthrow = rng.Next(1, 3);
-        Console.WriteLine("Wyrzuciłeś: " + randomthrow.ToString());
-        break;
-    case "2":
-        randomthrow = rng.Next(1, 4);
-        Console.WriteLine("Wyrzuciłeś: " + randomthrow.ToString());
-        break;
-    case "3":
-        randomthrow = rng.Next(1, 6);
-        Console.WriteLine("Wyrzuciłeś: " + randomthrow.ToString());
-        break;
-    case "4":
-        randomthrow = rng.Next(1, 20);
-        Console.WriteLine("Wyrzuciłeś: " + randomthrow.ToString());
-        break;
-    default:
-        Console.WriteLine("Podałeś złą opcje!");
-        break;
+    choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "1":
+            randomthrow = rng.Next(1, 3);
+            Console.WriteLine("Wyrzuciłeś: " + randomthrow.ToString());
+            break;
+        case "2":
+            randomthrow = rng.Next(1, 4);
+            Console.WriteLine("Wyrzuciłeś: " + randomthrow.ToString());
+            break;
+        case "3":
+            randomthrow = rng.Next(1, 6);
+            Console.WriteLine("Wyrzuciłeś: " + randomthrow.ToString());
+            break;
+        case "4":
+            randomthrow = rng.Next(1, 20);
+            Console.WriteLine("Wyrzuciłeś: " + randomthrow.ToString());
+            break;
+        case "q":
+            break;
+        default:
+            Console.WriteLine("Podałeś złą opcje!");
+            break;
+    }
 }
+while (choice != "q");
